c67c07b41479d9347d21572822118578262a0ed9
sagepoweron <108161754+sagepoweron@users.noreply.github.com>
2025-03-16 23:05:30 -0400

    Initial site

---
+++ javascript.js
@@ -0,0 +1,26 @@
+let _testButton = document.getElementById("testButton");
+
+//let message;
+//message = 'Welcome!';
+//alert(message); // shows the variable content
+
+//let number = prompt('Pick a number.', 18);
+//alert(number);
+
+//let result = multiply(3, 4);
+//alert(result);
+
+function ShowMessage()
+{
+    alert(message);
+}
+
+function Add7(number)
+{
+    return number + 7;
+}
+
+function Multiply(number1, number2)
+{
+    return number1 * number2;
+}
